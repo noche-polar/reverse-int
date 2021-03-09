1b7535bcefbdc39d5038f3cfb2495a3204092c80
noche-polar <kastiuchik.elena@gmail.com>
2021-03-09 20:14:18 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  if (n < 0) { n = n * -1; }
+
+  n = ("" + n).split('').map(Number);
+  n.reverse();
+  return +n.join("");
 }
